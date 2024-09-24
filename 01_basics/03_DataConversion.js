fc634d2fd60b5bcce21d9c4ab5a1f9ad42a15c9c
Harshit Luthra <115871845+harshit-11043@users.noreply.github.com>
2024-09-24 12:17:15 +0000

    Operations

--- 01_basics/03_DataConversion.js
+++ 01_basics/03_DataConversion.js
@@ -15,15 +15,47 @@
 //"33abc"=> NanN;
 //true=>1 false=>0
 
-let boolVal="ja";
-let isBooleanLoggedIn=Boolean(boolVal);
-console.log(isBooleanLoggedIn);
+// let boolVal="ja";
+// let isBooleanLoggedIn=Boolean(boolVal);
+// console.log(isBooleanLoggedIn);
 
-//1=> true
-// 0=> false
-//""=> false
-//"string"=> true
-let number=33
-let stringNumber=String(number)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// //1=> true
+// // 0=> false
+// //""=> false
+// //"string"=> true
+// let number=33
+// let stringNumber=String(number)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+/*                 Operations                */
+// let value=4
+// let negValue=-value
+// console.log(negValue);
+
+// console.log((2+2));
+// console.log((2-2));
+// console.log((2*2));
+// console.log((2**2));
+// console.log((2/3));
+// console.log((2%4));
+
+// let str1="hello"
+// let str2=" harshit";
+// console.log(str1+str2);
+
+// console.log(2+2+"2");
+
+console.log(+true);
+console.log(+"");
+let a=5;
+let b=++a;
+let c=a++;
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+
+
