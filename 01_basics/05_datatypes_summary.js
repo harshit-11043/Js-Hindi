7b55185802f6170d17ed11b10ec716ec81abca96
Harshit Luthra <115871845+harshit-11043@users.noreply.github.com>
2024-09-24 13:01:51 +0000

    Stack and Heap Memory

--- 01_basics/05_datatypes_summary.js
+++ 01_basics/05_datatypes_summary.js
@@ -7,28 +7,53 @@
 //undefined
 //symbol
 //bigInt
-const score=100
-const value=100.4
-const isLoggedIn=false
-const temp=null;
-let useremail=undefined;
+// const score=100
+// const value=100.4
+// const isLoggedIn=false
+// const temp=null;
+// let useremail=undefined;
 
-const id=Symbol('123')
-const anotherId=Symbol('123')
-console.log(id===anotherId);
+// const id=Symbol('123')
+// const anotherId=Symbol('123')
+// console.log(id===anotherId);
 
 //Non Primitive Call by reference
 //Objects, Array,functions
 
-const heros=["shaktiman","naagraj","doga"]
-let myObj={
-    name:"harshit",
-    age:21
-}
+// const heros=["shaktiman","naagraj","doga"]
+// let myObj={
+//     name:"harshit",
+//     age:21
+// }
 
-const myFunction=function(){
-    console.log("Hello World")
+// const myFunction=function(){
+//     console.log("Hello World")
+// }
+// console.log(typeof myObj)
+// console.log(typeof myFunction)
+// console.log(typeof heros);
+
+// ------------------------Memory Allocation--------------------------
+//Stack Memory
+//for Primitive data types
+
+
+
+
+//Heap Memory(for non primitve data types)
+
+let myYoutubeName="Harshitluthradotcom"
+
+let anotherName=myYoutubeName;
+anotherName="YoutubeHarshit";
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let user={
+    email:"user@google.com",
+    upi:"use@ohkhdfcbank.com"
 }
-console.log(typeof myObj)
-console.log(typeof myFunction)
-console.log(typeof heros);
+let userTwo=user
+userTwo.email="harshit@google.com"
+console.log(user);
+console.log(userTwo);
