9d662c4b90d33c89af3de925e0fb434dca46b9b0
Harshit Luthra <115871845+harshit-11043@users.noreply.github.com>
2024-09-24 11:22:33 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_DataConversion.js
@@ -0,0 +1,29 @@
+// let score ="harshit";
+// console.log(typeof score);
+// let valueinNumber=Number(score);
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
+
+// if(!isNaN(valueinNumber))
+// {
+//     console.log("Number Convertd");
+// }
+// else{
+//     console.log("Please give a valid input");
+// }
+// "33"=> 33
+//"33abc"=> NanN;
+//true=>1 false=>0
+
+let boolVal="ja";
+let isBooleanLoggedIn=Boolean(boolVal);
+console.log(isBooleanLoggedIn);
+
+//1=> true
+// 0=> false
+//""=> false
+//"string"=> true
+let number=33
+let stringNumber=String(number)
+console.log(stringNumber)
+console.log(typeof stringNumber)
