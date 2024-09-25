5662efd2ebd03aeacae02107970b1bf7b5641a23
Harshit Luthra <115871845+harshit-11043@users.noreply.github.com>
2024-09-25 06:15:50 +0000

    Nums and Maths

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,30 @@
+const score=400
+// console.log(score);
+// const balance= new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+const otherNumber=128.8902
+const newNum=otherNumber.toPrecision(4);
+// console.log(typeof newNum);
+const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// -----------------------------------MATHS-----------------------------------------
+// console.log(Math.abs(-5));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(1,2,3,4,5,6,7,8));
+// console.log(Math.max(12,494,2320,59,20,4950));
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
