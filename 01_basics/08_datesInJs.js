431b2f5dd9c5a54c27e5c8fc2b638844547174e3
Harshit Luthra <115871845+harshit-11043@users.noreply.github.com>
2024-09-25 07:02:52 +0000

    Dates in Js

---
+++ 01_basics/08_datesInJs.js
@@ -0,0 +1,27 @@
+//Dates
+
+let myDate=new Date()
+console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleString());
+
+// let mycreatedDate=new Date(2024,8,28);
+let mycreatedDate= new Date("08-23-2024")
+console.log(mycreatedDate.toDateString());
+
+let myTimeStamp= Date.now();
+
+// console.log(myTimeStamp);
+// console.log(mycreatedDate.getTime());
+console.log(Date.now()/1000);
+
+
+
+
+
+
+
