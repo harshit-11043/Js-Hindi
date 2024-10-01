9364aa043d336ad0a0d56c26f379583f60bdeb28
Harshit Luthra <115871845+harshit-11043@users.noreply.github.com>
2024-10-01 13:37:04 +0000

    Array in detail

---
+++ 02_basics/02_array.js
@@ -0,0 +1,26 @@
+const marvel_heroes=["thor","ironman","captain_america"];
+const dc_heroes=["superman","flash","batman"]
+// marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes);
+
+// const all_heroes=marvel_heroes.concat(dc_heroes);
+// console.log(all_heroes);
+
+// const all_new_heroes=[...marvel_heroes, ...dc_heroes]
+// console.log(all_new_heroes);
+const real_array=[1,2,3,[4,5,6],[7,8,[4,5,6]]]
+// const newone=real_array.flat(Infinity);
+// console.log(newone);
+console.log(Array.isArray("Harshit"));
+console.log(Array.from("Harshit"));
+console.log(Array.from({name:"Harshit"}));
+let score1=100;
+let score2=200;
+let score3=300;
+
+console.log(Array.of(score1, score2, score3));
+
+
+
+
+
