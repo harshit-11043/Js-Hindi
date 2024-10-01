6fedc20d573023d61586197cbb171cbc48329cb5
Harshit Luthra <115871845+harshit-11043@users.noreply.github.com>
2024-10-01 13:11:49 +0000

    Array basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,75 @@
+const myArr=[0,1,2,3,4,5,6];
+// console.log(myArr[0]);
+// const myArr1= new Array("shaktiman","hero","nagraj")
+// console.log(myArr1[1]);
+
+// Array mwthods
+// myArr.push(7);
+// console.log(myArr);
+// myArr.pop();
+// console.log(myArr);
+// // myArr.unshift(4);
+// myArr.shift();
+// console.log(myArr.includes(8));
+// console.log(myArr.indexOf(2));
+const newArr= myArr.join()
+// console.log(myArr);
+// // console.log(newArr);
+// console.log(typeof newArr);
+// console.log("A",myArr);
+// const nya1=myArr.slice(1,4);
+// console.log("B", myArr);
+// console.log(nya1);
+// const nya2=myArr.splice(1,4);
+// console.log("C",myArr);
+// console.log(nya2);
+
+
+
+// console.log(myArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("A",ARPIT);
+// const Harshit=ARPIT.slice(1,4);
+// const Shruti=ARPIT.splice(1,4);
+// console.log(Shruti);
+// console.log(ARPIT);
+
+
+
+// 2,3,4,5
+// 1,2,3,4,5,6,7
+
+
+//Shallow copy and deep copy
+
+const ARPIT=[1,2,3,4,5,6,7,9];
+
+// ARPIT.push(5);
+// // console.log(ARPIT);
+// // ARPIT.unshift(9);
+// ARPIT.pop();
+// ARPIT.shift();
+// console.log(ARPIT);
+
+const HARSHIT= ARPIT.join();
+console.log(HARSHIT);
+console.log(typeof HARSHIT);
+
+// 1,2,3,4,5,6,7,9,5
+// 1,2,3,4,5,6,7,5
